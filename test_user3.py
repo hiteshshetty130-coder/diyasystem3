b2bd8f0982c34ed342c7b9dc3a83fc6ae001aba4
hiteshshetty130-coder <hiteshshetty130@gmail.com>
2025-12-09 07:05:45 +0530

    added comments

--- test_user3.py
+++ test_user3.py
@@ -11,7 +11,7 @@ class TestExcelFileExtraction(unittest.TestCase):
     #Verify Excel file Download
     @patch("user3.requests.get")
     def test_case1(self,mock_get):
-        
+        #create a fake excel sheet
         wb=Workbook()
         ws=wb.active
         ws['a1']="hello"
@@ -19,13 +19,15 @@ class TestExcelFileExtraction(unittest.TestCase):
         ws['b1']=100
         ws["b2"]=200
 
-        excel_bytes=BytesIO()
+        excel_bytes=BytesIO() #fake Bytes 
         wb.save(excel_bytes)
 
         zip_bytes=BytesIO()
         with zipfile.ZipFile(zip_bytes,"w") as z_f:
-            z_f.writestr("output_mock.xlsx",excel_bytes.getvalue())
+            z_f.writestr("output_mock.xlsx",excel_bytes.getvalue()) #creating a fake zipe file with excel file stored in it
 
+
+        #mock the url with the content of fake zip file url
         mock_response=Mock()
         mock_response.status_code=200
         mock_response.content=zip_bytes.getvalue()
@@ -33,9 +35,11 @@ class TestExcelFileExtraction(unittest.TestCase):
 
         df,file_path=main()
 
+        #chcek if dataframe returns something and the file path exists
         self.assertIsNotNone(df)
         self.assertTrue(os.path.exists(file_path))
         
+    #verify Excel file extraction
     @patch("user3.requests.get")
     def test_case2(self,mock_get):
         wb=Workbook()
@@ -59,10 +63,12 @@ class TestExcelFileExtraction(unittest.TestCase):
 
         df, file_path = main()
 
+        #check if file path ends is correct,check the length of rows and columns are greater than one
         self.assertTrue(file_path.endswith(".xlsx"))
         self.assertGreater(len(df.columns),0)
         self.assertGreater(len(df),0)
 
+    #validate the file type and format
     @patch("user3.requests.get")
     def test_case3(self, mock_get):
         wb = Workbook()
@@ -86,19 +92,22 @@ class TestExcelFileExtraction(unittest.TestCase):
 
         df, file_path = main()
 
+        #check file type , if the path exists, and also check if the dataframe returned or not
         self.assertTrue(file_path.endswith(".xlsx"))
         self.assertTrue(os.path.exists(file_path))
         self.assertIsInstance(df,pd.DataFrame)
 
+    #handle Data Structure
     @patch("user3.requests.get")
     def test_case4(self, mock_get):
+        #create a fake dataframe to check if all the expected columns exists or not
         df_fake=pd.DataFrame({"Employee ID":["E1011"],"Full Name":["Hitesh"],"Job Title":["Manager"],"Department":["IT"],
                               "Business Unit":["xYZ"],"Gender":["male"],"Ethnicity":["asian"],"Age":[19],"Hire Date":["19-12-2005"],
                               "Annual Salary":[20000],"Bonus":[5000],"Country":["india"],"City":["mangalore"],
                               "Exit Date":["19-12-2024"]})
 
         excel_bytes = BytesIO()
-        df_fake.to_excel(excel_bytes)
+        df_fake.to_excel(excel_bytes) #convert to excel
         excel_bytes.seek(0)
     
 
@@ -114,19 +123,23 @@ class TestExcelFileExtraction(unittest.TestCase):
 
         df, file_path = main()
 
+        #check if all the required columns exists or not
         expected_columns=["Employee ID","Full Name","Job Title","Hire Date"]
 
         for col in expected_columns:
             self.assertIn(col,df.columns)
     
+    #Handle missing and invalid data
     @patch("user3.requests.get")
     def test_case5(self, mock_get):
+        #same create dataframe to check if any columns are empty
         df_fake = pd.DataFrame(
             {"Employee ID": ["E1011"], "Full Name": ["Hitesh"], "Job Title": ["Manager"], "Department": ["IT"],
              "Business Unit": ["xYZ"], "Gender": ["male"], "Ethnicity": ["asian"], "Age": [19],
              "Hire Date": ["19-12-2005"],
              "Annual Salary": [20000], "Bonus": [5000], "Country": ["india"], "City": ["mangalore"],
              "Exit Date": ["19-12-2024"]})
+        
 
         excel_bytes = BytesIO()
         df_fake.to_excel(excel_bytes)
@@ -146,6 +159,7 @@ class TestExcelFileExtraction(unittest.TestCase):
 
         expected_columns = ["Employee ID", "Full Name", "Job Title", "Hire Date"]
 
+        #chcek if any column is having missing values and also null values
         for col in expected_columns:
             self.assertIn(col, df.columns,"columns is missing")
 
--- user3.py
+++ user3.py
@@ -9,27 +9,31 @@ logging.basicConfig(level=logging.INFO)
 def main():
     retry=3
 
+    #loop Till the maximum attempts Reaches 
     for attempt in range(1,retry+1):
         try:
-            url="https://www.thespreadsheetguru.com/wp-content/uploads/2022/12/EmployeeSampleData.zip"
+            url="https://www.thespreadsheetguru.com/wp-content/uploads/2022/12/EmployeeSampleData.zip" 
             response=requests.get(url)
             file_path="output.xlsx"
 
+            #IO.bytesIO is used to handle binary data returned from the url
             zipe_file_path=io.BytesIO(response.content)
-            with zipfile.ZipFile(zipe_file_path,"r") as z:
+            with zipfile.ZipFile(zipe_file_path,"r") as z: #using zipefile module open in read mode all the files inside 
                 try:
+                    #fetch the excel file
                     excel_file=next(f for f in z.namelist() if f.endswith(".xlsx"))   
                     extracted_data=z.open(excel_file)
-                    df=pd.read_excel(extracted_data)
+                    df=pd.read_excel(extracted_data)        #create a dataframe from the file extracted
 
-                except Exception as e:
+                except Exception as e: #if file cannot be extracted then log exception
                     logging.exception("Corrupted File!!") 
 
-
+                #check for supported file format 
                 if not excel_file.endswith(".xlsx"):
                     logging.error("Unsupported File Format!")
 
-            df.to_excel(file_path,index=False)
+            df.to_excel(file_path,index=False) #convert to excel
+
             if not file_path.endswith(".xlsx"):
                 logging.error("Incorrect File Type!!")
             
@@ -38,6 +42,8 @@ def main():
             return df,"output.xlsx"
             break
 
+
+        #exception handling for invalid ZIP File,Connection problem,and any unexcepted error handling
         except zipfile.BadZipFile:
             logging.exception("Sorry Inavlid Zip file!!")
 
